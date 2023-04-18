7ec98a3ffb1155de96787ffb1ed6cb1d42d2dd4f
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-04-18 23:18:02 +0300

    install cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const EmployeeModel = require("./db/employee.model");
 
-const { MONGO_URL, PORT = 8080 } = process.env;
+const { MONGO_URL, PORT } = process.env;
 
 if (!MONGO_URL) {
   console.error("Missing MONGO_URL environment variable");
@@ -12,6 +13,16 @@ if (!MONGO_URL) {
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/api/employees/", async (req, res) => {
   const employees = await EmployeeModel.find().sort({ created: "desc" });

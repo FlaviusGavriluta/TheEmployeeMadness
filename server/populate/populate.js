842d96cc673ad6fff3758a452b2110a4032ad49f
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-04-20 00:23:16 +0300

    populated equipments document in database

--- server/populate/populate.js
+++ server/populate/populate.js
@@ -3,9 +3,12 @@ Loading the .env file and creates environment variables from it
 */
 require("dotenv").config();
 const mongoose = require("mongoose");
-const names = require("./employee/names.json");
-const levels = require("./employee/levels.json");
-const positions = require("./employee/positions.json");
+const employeesName = require("./employee/names.json");
+const employeesLevel = require("./employee/levels.json");
+const employeesPosition = require("./employee/positions.json");
+const equipmentsName = require("./equipment/names.json");
+const equipmentTypes = require("./equipment/types.json");
+const amounts = require("./equipment/amounts.json");
 const EmployeeModel = require("../db/employee.model");
 const EquipmentModel = require("../db/equipment.model");
 
@@ -21,20 +24,34 @@ const pick = (from) => from[Math.floor(Math.random() * (from.length - 0))];
 const populateEmployees = async () => {
   await EmployeeModel.deleteMany({});
 
-  const employees = names.map((name) => ({
+  const employees = employeesName.map((name) => ({
     name,
-    level: pick(levels),
-    position: pick(positions),
+    level: pick(employeesLevel),
+    position: pick(employeesPosition),
   }));
 
   await EmployeeModel.create(...employees);
   console.log("Employees created");
 };
 
+const populateEquipment = async () => {
+  await EquipmentModel.deleteMany({});
+
+  const equipments = equipmentsName.map((name, i) => ({
+    name,
+    type: equipmentTypes[i],
+    amount: amounts[i],
+  }));
+
+  await EquipmentModel.create(...equipments);
+  console.log("Equipment created");
+};
+
 const main = async () => {
   await mongoose.connect(mongoUrl);
 
   await populateEmployees();
+  await populateEquipment();
 
   await mongoose.disconnect();
 };

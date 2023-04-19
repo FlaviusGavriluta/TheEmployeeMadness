3be8df29b249eae8adcf7eb36cd12074a542001c
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-04-20 00:39:46 +0300

    clean server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const { connect } = require("mongoose");
 const employeeRoutes = require("./routes/employeeRoutes");
 const equipmentRoutes = require("./routes/equipmentRoutes");
 
@@ -12,28 +12,24 @@ if (!MONGO_URL) {
   process.exit(1);
 }
 
+//Middleware
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
+//Routing
 app.use("/api/employees", employeeRoutes);
 app.use("/api/equipments", equipmentRoutes);
 
+//Connect to MongoDB and Start the Server
 const main = async () => {
   try {
-    await mongoose.connect(MONGO_URL);
+    await connect(MONGO_URL).then(() =>
+      console.log("Successfully connected to MongoDB!")
+    );
     app.listen(PORT, () => {
-      console.log(`App is listening on ${PORT}`);
-      console.log("Try /api/employees route right now");
+      console.log(`Server listening on http://localhost:${PORT}`);
+      console.log("Try /api/employees or /api/equipments route right now");
     });
   } catch (err) {
     console.error(err);

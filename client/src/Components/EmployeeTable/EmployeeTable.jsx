cdffeb19986ed66a9ebccf1ffb33d09dd6192bf8
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-04-19 18:49:42 +0300

    Extend the list with arrangement features

--- client/src/Components/EmployeeTable/EmployeeTable.jsx
+++ client/src/Components/EmployeeTable/EmployeeTable.jsx
@@ -2,30 +2,58 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import "./EmployeeTable.css";
 import { SearchBy } from "./SearchBy";
+import { SortBy } from "./SortBy";
 
 const EmployeeTable = ({ employees, onDelete }) => {
   const [search, setSearch] = useState("");
+  const [sortBy, setSortBy] = useState("firstName");
+  const [sortOrder, setSortOrder] = useState("asc");
+
+  const parseName = (name) => {
+    const names = name.split(" ");
+    const firstName = names[0];
+    const middleName = names.length > 2 ? names.slice(1, -1).join(" ") : "";
+    const lastName = names[names.length - 1];
+    return { firstName, middleName, lastName };
+  };
 
   const filteredEmployees = employees
-    ? employees.filter((employee) =>
-        [employee.level, employee.position].some((field) =>
-          field.toLowerCase().includes(search.toLowerCase())
+    ? employees
+        .filter((employee) =>
+          [employee.level, employee.position].some((field) =>
+            field.toLowerCase().includes(search.toLowerCase())
+          )
         )
-      )
+        .sort((a, b) => {
+          let comparison = 0;
+          switch (sortBy) {
+            case "level":
+            case "position": {
+              const valueA = a[sortBy].toLowerCase();
+              const valueB = b[sortBy].toLowerCase();
+              comparison = valueA.localeCompare(valueB);
+              break;
+            }
+            default: {
+              const nameA = parseName(a.name)[sortBy].toLowerCase();
+              const nameB = parseName(b.name)[sortBy].toLowerCase();
+              comparison = nameA.localeCompare(nameB);
+              break;
+            }
+          }
+          return sortOrder === "asc" ? comparison : -comparison;
+        })
     : [];
 
   return (
     <div className="EmployeeTable">
       <SearchBy onSearch={(search) => setSearch(search)} />
       <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Level</th>
-            <th>Position</th>
-            <th />
-          </tr>
-        </thead>
+        <SortBy
+          setSortBy={setSortBy}
+          sortOrder={sortOrder}
+          setSortOrder={setSortOrder}
+        />
         <tbody>
           {filteredEmployees.map((employee) => (
             <tr key={employee._id}>
--- client/src/Components/EmployeeTable/SortBy.jsx
+++ client/src/Components/EmployeeTable/SortBy.jsx
@@ -1,1 +1,85 @@
-export const SortBy = () => {};
+export const SortBy = ({ setSortBy, sortOrder, setSortOrder }) => {
+  const toggleSortOrder = () => {
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  };
+  return (
+    <thead>
+      <tr>
+        <th>
+          <div className="dropdown">
+            <a
+              className="dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <button className="btn m-0 p-0">Name</button>
+            </a>
+            <ul className="dropdown-menu">
+              <li>
+                <a
+                  className="dropdown-item"
+                  href="#"
+                  onClick={() => {
+                    setSortBy("firstName");
+                    toggleSortOrder();
+                  }}
+                >
+                  First name{" "}
+                </a>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item"
+                  href="#"
+                  onClick={() => {
+                    setSortBy("lastName");
+                    toggleSortOrder();
+                  }}
+                >
+                  Last name{" "}
+                </a>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item"
+                  href="#"
+                  onClick={() => {
+                    setSortBy("middleName");
+                    toggleSortOrder();
+                  }}
+                >
+                  Middle name{" "}
+                </a>
+              </li>
+            </ul>
+          </div>
+        </th>
+        <th>
+          <button
+            className="btn m-0 p-0"
+            onClick={() => {
+              setSortBy("level");
+              toggleSortOrder();
+            }}
+          >
+            Level
+          </button>
+        </th>
+        <th>
+          <button
+            className="btn m-0 p-0"
+            onClick={() => {
+              setSortBy("position");
+              toggleSortOrder();
+            }}
+          >
+            Position
+          </button>
+        </th>
+        <th />
+      </tr>
+    </thead>
+  );
+};

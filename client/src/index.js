06111012a826f2e5f9f96f08ac563926b2bbf086
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-04-20 18:59:54 +0300

    repair route

--- client/src/index.js
+++ client/src/index.js
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
         element: <EmployeeList />,
       },
       {
-        path: "/name/:search",
+        path: "/employees/:search",
         element: <EmployeeSearchList />,
       },
       {

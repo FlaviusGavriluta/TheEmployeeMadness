59ebc01c4c72ae3e9e6d5a35016de9617bd16abc
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-04-21 05:18:24 +0300

    change Layout and styling

--- client/src/Pages/Layout/Layout.jsx
+++ client/src/Pages/Layout/Layout.jsx
@@ -1,32 +1,93 @@
-import { Outlet, Link } from "react-router-dom";
-
 import "./Layout.css";
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => (
-  <div className="Layout">
-    <nav>
-      <ul>
-        <li className="">
-          <Link to="/">Employees</Link>
-        </li>
-        <li>/</li>
-        <li className="grow">
-          <Link to="/equipmentsList">Equipments</Link>
-        </li>
-        <li>
-          <Link to="/createEquipment">
-            <button className="btn btn-light btn-sm">Create Equipment</button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/create">
-            <button className="btn btn-light btn-sm">Create Employee</button>
-          </Link>
-        </li>
-      </ul>
-    </nav>
-    <Outlet />
-  </div>
+  <>
+    <div className="sidebar">
+      <div className="sidebar-brand">
+        <div className="mt-2">
+          <img
+            src="Codecool.png"
+            alt="logo"
+            width="27"
+            height="27"
+            className="logo ms-3 mb-3"
+          />
+          <div
+            className="text-container d-inline-block ms-3"
+            style={{ fontFamily: "sans-serif" }}
+          >
+            <p className="mb-0 p-0" style={{ fontSize: "16px" }}>
+              <b>JOURNEY</b>
+            </p>
+            <p style={{ lineHeight: "0.6", fontSize: "8px" }}>
+              <b>BY CODECOOL</b>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="sidebar-menu text-light ">
+        <ul>
+          <li>
+            <a className="active">
+              <span className="las la-home"></span> <span>Dashboard</span>
+            </a>
+          </li>
+          <li>
+            <a className="">
+              <span className="las la-users"></span> <span>Employees</span>
+            </a>
+          </li>
+          <li>
+            <a className="">
+              <span className="las la-tools"></span> <span>Equipments</span>
+            </a>
+          </li>
+          <li>
+            <a className="">
+              <span className="las la-concierge-bell"></span>
+              <span>Attendance</span>
+            </a>
+          </li>
+          <li>
+            <a className="">
+              <span className="las la-tasks"></span> <span>Tasks</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div className="main-content">
+      <header>
+        <h3 style={{ color: "#222" }}>
+          <label htmlFor="">
+            <span
+              className="btn m-0 px-3 las la-bars"
+              style={{ fontSize: "1.7rem", color: "#222" }}
+            ></span>
+          </label>
+          Dashboard
+        </h3>
+        <div className="search-wrapper">
+          <span className="las la-search"></span>
+          <input type="search" placeholder="Search here" />
+        </div>
+        <div className="user-wrapper">
+          <img src="user.png" width={"40px"} height={"40px"} />
+          <div>
+            <h5 style={{ fontSize: "1rem" }}>
+              <b>Flavius Dev</b>
+            </h5>
+            <small>Super admin</small>
+          </div>
+        </div>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  </>
 );
 
 export default Layout;

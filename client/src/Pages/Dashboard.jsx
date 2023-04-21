769b9c4b62aec1c9a868e4187e0eaf68dff6dc83
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-04-21 05:17:13 +0300

    add a new homepage

---
+++ client/src/Pages/Dashboard.jsx
@@ -0,0 +1,45 @@
+const Dashboard = () => (
+  <div className="cards">
+    <div className="card-single">
+      <div>
+        <h1>111</h1>
+        <span>Employees</span>
+      </div>
+      <div>
+        <span className="las la-users"></span>
+      </div>
+    </div>
+
+    <div className="card-single">
+      <div>
+        <h1>78%</h1>
+        <span>Attendance</span>
+      </div>
+      <div>
+        <span className="las la-concierge-bell"></span>
+      </div>
+    </div>
+
+    <div className="card-single">
+      <div>
+        <h1>78%</h1>
+        <span>Attendance</span>
+      </div>
+      <div>
+        <span className="las la-concierge-bell"></span>
+      </div>
+    </div>
+
+    <div className="card-single">
+      <div>
+        <h1>78%</h1>
+        <span>Attendance</span>
+      </div>
+      <div>
+        <span className="las la-concierge-bell"></span>
+      </div>
+    </div>
+  </div>
+);
+
+export default Dashboard;

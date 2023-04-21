04fb5d156523838637ddb1468d834188e572f73d
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-04-21 19:11:28 +0300

    refactor and style Layout

--- client/src/Pages/Layout/Layout.jsx
+++ client/src/Pages/Layout/Layout.jsx
@@ -1,11 +1,14 @@
 import "./Layout.css";
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => (
   <>
     <div className="sidebar">
-      <div className="sidebar-brand">
+      <div
+        className="sidebar-brand text-light"
+        style={{ padding: "1rem 0rem 1rem 0.7rem", height: "90px" }}
+      >
         <div className="mt-2">
           <img
             src="Codecool.png"
@@ -27,60 +30,81 @@ const Layout = () => (
           </div>
         </div>
       </div>
-      <div className="sidebar-menu text-light ">
+      <div className="sidebar-menu" style={{ marginTop: "1rem" }}>
         <ul>
-          <li>
-            <a className="active">
-              <span className="las la-home"></span> <span>Dashboard</span>
-            </a>
+          <li className="onActive">
+            <Link to={"/"} style={{ textDecoration: "none" }}>
+              <button className="btn">
+                <span className="las la-home"></span> <span>Dashboard</span>
+              </button>
+            </Link>
           </li>
           <li>
-            
-            <a className="">
-              <span className="las la-users"></span> <span>Employees</span>
-            </a>
+            <Link to={"/employees"} style={{ textDecoration: "none" }}>
+              <button className="btn">
+                <span className="las la-users"></span> <span>Employees</span>
+              </button>
+            </Link>
           </li>
           <li>
-            
-            <a className="">
-              <span className="las la-tools"></span> <span>Equipments</span>
-            </a>
+            <Link to={"/equipments"} style={{ textDecoration: "none" }}>
+              <button className="btn">
+                <span className="las la-tools"></span> <span>Equipments</span>
+              </button>
+            </Link>
           </li>
           <li>
-            
-            <a className="">
-              <span className="las la-concierge-bell"></span>
-              <span>Attendance</span>
-            </a>
+            <Link to={"/missing"} style={{ textDecoration: "none" }}>
+              <button className="btn">
+                <span className="las la-concierge-bell"></span>
+                <span>Attendance</span>
+              </button>
+            </Link>
           </li>
           <li>
-            <a className="">
-              <span className="las la-tasks"></span> <span>Tasks</span>
-            </a>
+            <Link to={"/tasks"} style={{ textDecoration: "none" }}>
+              <button className="btn">
+                <span className="las la-tasks"></span> <span>Tasks</span>
+              </button>
+            </Link>
           </li>
         </ul>
       </div>
     </div>
     <div className="main-content">
       <header>
-        <h3 style={{ color: "#222" }}>
+        <h3 className="fw-bold" style={{ color: "#222" }}>
           <label htmlFor="">
-            <span
-              className="btn m-0 px-3 las la-bars"
-              style={{ fontSize: "1.7rem", color: "#222" }}
-            ></span>
+            <Link to={"/"} style={{ textDecoration: "none" }}>
+              <span
+                className="btn m-0 px-3 las la-bars"
+                style={{ fontSize: "1.7rem", color: "#222" }}
+              ></span>
+            </Link>
           </label>
           Dashboard
         </h3>
-        <div className="search-wrapper">
-          <span className="las la-search"></span>
-          <input type="search" placeholder="Search here" />
+
+        <div className="input-group" style={{ width: "250px", height: "20px" }}>
+          <input
+            type="search"
+            className="form-control rounded-start-pill"
+            placeholder="Search here"
+          />
+          <button
+            className="btn btn-light border-end border-top border-bottom rounded-end-pill"
+            type="button"
+            id="button-addon1"
+          >
+            <span className="las la-search"></span>
+          </button>
         </div>
+
         <div className="user-wrapper">
           <img src="user.jpg" width={"40px"} height={"40px"} alt="user" />
           <div>
-            <h5 style={{ fontSize: "1rem" }}>
-              <b>Flavius Dev</b>
+            <h5 className="m-0" style={{ fontSize: "1rem" }}>
+              <b>Flavius Gav.</b>
             </h5>
             <small>Software developer</small>
           </div>

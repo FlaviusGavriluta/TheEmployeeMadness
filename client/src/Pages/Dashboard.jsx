f86f9248edac5326b294966cfc0318adf3bfd706
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-04-22 11:40:00 +0300

    feat: Add responsive design to page

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -60,52 +60,54 @@ const Dashboard = () => (
             <h6 className="fw-bold my-1">Recent Tasks</h6>
           </div>
           <div className="card-body my-1">
-            <table>
-              <thead>
-                <tr>
-                  <td>Project Tasks</td>
-                  <td>Status</td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Filter features</td>
-                  <td>
-                    <span className="status purple"></span> review
-                  </td>
-                </tr>
-                <tr>
-                  <td>Arrangement features</td>
-                  <td>
-                    <span className="status purple"></span> review
-                  </td>
-                </tr>
-                <tr>
-                  <td>Add Equipment</td>
-                  <td>
-                    <span className="status purple"></span> review
-                  </td>
-                </tr>
-                <tr>
-                  <td>The Search</td>
-                  <td>
-                    <span className="status purple"></span> review
-                  </td>
-                </tr>
-                <tr>
-                  <td>Employee Attendance</td>
-                  <td>
-                    <span className="status info"></span> in progress
-                  </td>
-                </tr>
-                <tr>
-                  <td>Assign equipment to any employee</td>
-                  <td>
-                    <span className="status orange"></span> pending
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <div className="table-responsive">
+              <table>
+                <thead>
+                  <tr>
+                    <td>Project Tasks</td>
+                    <td>Status</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Filter features</td>
+                    <td>
+                      <span className="status purple"></span> review
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Arrangement features</td>
+                    <td>
+                      <span className="status purple"></span> review
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Add Equipment</td>
+                    <td>
+                      <span className="status purple"></span> review
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>The Search</td>
+                    <td>
+                      <span className="status purple"></span> review
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Employee Attendance</td>
+                    <td>
+                      <span className="status info"></span> in progress
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Assign equipment to any employee</td>
+                    <td>
+                      <span className="status orange"></span> pending
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
--- client/src/Pages/Layout/Layout.jsx
+++ client/src/Pages/Layout/Layout.jsx
@@ -4,10 +4,11 @@ import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => (
   <>
+    <input type="checkbox" id="nav-toggle" />
     <div className="sidebar">
       <div
         className="sidebar-brand text-light"
-        style={{ padding: "1rem 0rem 1rem 0.7rem", height: "90px" }}
+        style={{ padding: "1rem 0rem 1rem 0.5rem", height: "90px" }}
       >
         <div className="mt-2">
           <img
@@ -18,14 +19,17 @@ const Layout = () => (
             className="logo ms-3 mb-3"
           />
           <div
-            className="text-container d-inline-block ms-3"
+            className="logo text-container d-inline-block ms-3"
             style={{ fontFamily: "sans-serif" }}
           >
-            <p className="mb-0 p-0" style={{ fontSize: "16px" }}>
-              <b>JOURNEY</b>
+            <p className="mb-0 p-0 fw-bold" style={{ fontSize: "16px" }}>
+              JOURNEY
             </p>
-            <p style={{ lineHeight: "0.6", fontSize: "8px" }}>
-              <b>BY CODECOOL</b>
+            <p
+              className="fw-bold"
+              style={{ lineHeight: "0.6", fontSize: "8px" }}
+            >
+              BY CODECOOL
             </p>
           </div>
         </div>
@@ -81,18 +85,19 @@ const Layout = () => (
     <div className="main-content">
       <header>
         <h3 className="fw-bold" style={{ color: "#222" }}>
-          <label htmlFor="">
-            <Link to={"/"} style={{ textDecoration: "none" }}>
-              <span
-                className="btn m-0 px-3 las la-bars"
-                style={{ fontSize: "1.7rem", color: "#222" }}
-              ></span>
-            </Link>
+          <label className="dashboardIcon" htmlFor="nav-toggle">
+            <span
+              className="btn m-0 px-3 las la-bars"
+              style={{ fontSize: "1.7rem", color: "#222" }}
+            ></span>
           </label>
           Dashboard
         </h3>
 
-        <div className="input-group" style={{ width: "250px", height: "20px" }}>
+        <div
+          className="search-wrapper input-group"
+          style={{ width: "250px", height: "20px" }}
+        >
           <input
             type="search"
             className="form-control rounded-start-pill"
@@ -105,6 +110,19 @@ const Layout = () => (
           >
             <span className="las la-search"></span>
           </button>
+        </div>
+
+        <div className="btn border-0 searching d-none">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-search"
+            viewBox="0 0 14 14"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
         </div>
 
         <div className="user-wrapper">

300de9aeb6908bdab86144d4135018add43705ed
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-04-23 19:00:50 +0300

    feat: add structure HtmL for a container

--- client/src/Components/EquipmentTable/EquipmentTable.jsx
+++ client/src/Components/EquipmentTable/EquipmentTable.jsx
@@ -4,33 +4,37 @@ import { Edit } from "../Buttons/Edit";
 import { Delete } from "../Buttons/Delete";
 
 const EquipmentTable = ({ equipments, onDelete }) => (
-  <div className="EquipmentTable">
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Type</th>
-          <th>Amount</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        {equipments.map((equipment) => (
-          <tr key={equipment._id}>
-            <td>{equipment.name}</td>
-            <td>{equipment.type}</td>
-            <td>{equipment.amount}</td>
-            <td>
-              <Link to={`/updateEquipment/${equipment._id}`}>
-                <Edit />
-              </Link>
-              <Delete onDelete={() => onDelete(equipment._id)} />
-            </td>
+  <section className="EquipmentTable">
+    <div className="container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Actions</th>
+            <th />
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
+        </thead>
+        <tbody>
+          {equipments.map((equipment) => (
+            <tr key={equipment._id}>
+              <td>{equipment.name}</td>
+              <td>{equipment.type}</td>
+              <td>{equipment.amount}</td>
+              <td>
+                <i className="btn fa fa-ellipsis-v" aria-hidden="true"></i>
+                <Link to={`/updateEquipment/${equipment._id}`}>
+                  <Edit />
+                </Link>
+                <Delete onDelete={() => onDelete(equipment._id)} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </section>
 );
 
 export default EquipmentTable;

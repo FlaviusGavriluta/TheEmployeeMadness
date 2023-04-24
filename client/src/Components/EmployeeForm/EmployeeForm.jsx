84d73b9b632ac042fe5b47903ee1d9a5620bc4c6
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-04-24 07:41:08 +0300

    feat: style form

--- client/src/Components/EmployeeForm/EmployeeForm.jsx
+++ client/src/Components/EmployeeForm/EmployeeForm.jsx
@@ -1,3 +1,5 @@
+import "./EmployeeForm.css";
+
 const EmployeeForm = ({ onSave, disabled, employee, onCancel }) => {
   const onSubmit = (e) => {
     e.preventDefault();
@@ -13,50 +15,59 @@ const EmployeeForm = ({ onSave, disabled, employee, onCancel }) => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      {employee && (
-        <input type="hidden" name="_id" defaultValue={employee._id} />
-      )}
-
-      <div className="control">
-        <label htmlFor="name">Name:</label>
-        <input
-          defaultValue={employee ? employee.name : null}
-          name="name"
-          id="name"
-          className="form-control"
-        />
-      </div>
-
-      <div className="control">
-        <label htmlFor="level">Level:</label>
-        <input
-          defaultValue={employee ? employee.level : null}
-          name="level"
-          id="level"
-          className="form-control"
-        />
-      </div>
-
-      <div className="control">
-        <label htmlFor="position">Position:</label>
-        <input
-          defaultValue={employee ? employee.position : null}
-          name="position"
-          id="position"
-          className="form-control"
-        />
-      </div>
-
-      <div className="buttons">
-        <button className="btn btn-primary" type="submit" disabled={disabled}>
-          {employee ? "Update Employee" : "Create Employee"}
-        </button>
-        <button className="btn btn-danger" type="button" onClick={onCancel}>
-          Cancel
-        </button>
-      </div>
-    </form>
+    <div className="mainForm">
+      <form onSubmit={onSubmit}>
+        {employee && (
+          <input type="hidden" name="_id" defaultValue={employee._id} />
+        )}
+        <h2 className="textForm text-dark text-center mx-3 mt-3">
+          Create Employee
+        </h2>
+        <div className="inputBox">
+          <input
+            defaultValue={employee ? employee.name : null}
+            name="name"
+            id="name"
+            required="required"
+          />
+          <span>Name</span>
+        </div>
+        <div className="inputBox">
+          <input
+            defaultValue={employee ? employee.level : null}
+            name="level"
+            id="level"
+            required="required"
+          />
+          <span>Level</span>
+        </div>
+        <div className="inputBox">
+          <input
+            defaultValue={employee ? employee.position : null}
+            name="position"
+            id="position"
+            required="required"
+          />
+          <span>Position</span>
+        </div>
+        <div className="buttons pe-3 m-0 text-center">
+          <button
+            className="btn btn-info mt-5 p-2 mx-3"
+            type="submit"
+            disabled={disabled}
+          >
+            {employee ? "Update Employee" : "Create Employee"}
+          </button>
+          <button
+            className="btn btn-secondary mt-5 p-2 mx-1"
+            type="button"
+            onClick={onCancel}
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- client/src/Components/EquipmentForm/EquipmentForm.jsx
+++ client/src/Components/EquipmentForm/EquipmentForm.jsx
@@ -1,4 +1,4 @@
-import "./EquipmentForm.css"
+import "./EquipmentForm.css";
 
 const EquipmentForm = ({ onSave, disabled, equipment, onCancel }) => {
   const onSubmit = (e) => {
@@ -15,50 +15,59 @@ const EquipmentForm = ({ onSave, disabled, equipment, onCancel }) => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      {equipment && (
-        <input type="hidden" name="_id" defaultValue={equipment._id} />
-      )}
-
-      <div className="control">
-        <label htmlFor="name">Name:</label>
-        <input
-          defaultValue={equipment ? equipment.name : null}
-          name="name"
-          id="name"
-          className="form-control"
-        />
-      </div>
-
-      <div className="control">
-        <label htmlFor="type">Type:</label>
-        <input
-          defaultValue={equipment ? equipment.type : null}
-          name="type"
-          id="type"
-          className="form-control"
-        />
-      </div>
-
-      <div className="control">
-        <label htmlFor="amount">Amount:</label>
-        <input
-          defaultValue={equipment ? equipment.amount : null}
-          name="amount"
-          id="amount"
-          className="form-control"
-        />
-      </div>
-
-      <div className="buttons">
-        <button className="btn btn-primary" type="submit" disabled={disabled}>
-          {equipment ? "Update Equipment" : "Create Equipment"}
-        </button>
-        <button className="btn btn-danger" type="button" onClick={onCancel}>
-          Cancel
-        </button>
-      </div>
-    </form>
+    <div className="mainForm">
+      <form onSubmit={onSubmit}>
+        {equipment && (
+          <input type="hidden" name="_id" defaultValue={equipment._id} />
+        )}
+        <h2 className="textForm text-dark text-center mx-3 mt-3">
+          Create Equipment
+        </h2>
+        <div className="inputBox">
+          <input
+            defaultValue={equipment ? equipment.name : null}
+            name="name"
+            id="name"
+            required="required"
+          />
+          <span>Name</span>
+        </div>
+        <div className="inputBox">
+          <input
+            defaultValue={equipment ? equipment.type : null}
+            name="type"
+            id="type"
+            required="required"
+          />
+          <span>Type</span>
+        </div>
+        <div className="inputBox">
+          <input
+            defaultValue={equipment ? equipment.amount : null}
+            name="amount"
+            id="amount"
+            required="required"
+          />
+          <span>Amount</span>
+        </div>
+        <div className="buttons pe-3 m-0 text-center">
+          <button
+            className="btn btn-info mt-5 p-2 mx-3"
+            type="submit"
+            disabled={disabled}
+          >
+            {equipment ? "Update Equipment" : "Create Equipment"}
+          </button>
+          <button
+            className="btn btn-secondary mt-5 p-2 mx-1"
+            type="button"
+            onClick={onCancel}
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
